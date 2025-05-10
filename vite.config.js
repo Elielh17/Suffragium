79b6d14e2037a8dc665fa28ee9ae71a5647d8931
Elielh17 <eliel.hernandez1@upr.edu>
2025-05-10 16:53:48 -0400

    fix for vite config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Suffragium/', // ← this must match your repo name
+  base: '/', // ✅ root path for custom domain
   plugins: [react()],
 });
