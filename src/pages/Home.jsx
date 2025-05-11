6f0fa43787d74ac21f659d9de5e3ba039f487a63
Elielh17 <eliel.hernandez1@upr.edu>
2025-05-10 23:03:28 -0400

    Added Logo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Home.css"; // Import CSS file
 import ProtectedRoute from "../components/ProtectedRoute";
+import logo from "../assets/Suffragium-logo.jpg";
 
 const Home = () => {
     return (
         <div className="home-container">
+
+            <img src={logo} alt="Suffragium Logo" className="home-logo" />
+
             <h1 className="home-title">Welcome to Suffragium</h1>
             <p className="home-description">
                 A secure and transparent online voting platform. Join or create an election today!
